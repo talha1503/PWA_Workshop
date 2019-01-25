901bd1d1c985270deeb97fe9abb98866ff2a7b77
talha1503 <42352729+talha1503@users.noreply.github.com>
2019-01-25 19:33:57 +0530

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -61,14 +61,4 @@ async function networkFirst(request)
     }
 
 }
-window.addEventListener("load",(e) => {
-    console.log("called");
-	if ('serviceWorker' in navigator) {
-		try {
-			navigator.serviceWorker.register('./serviceworker.js');
-			console.log('SW registered');
-		} catch (error) {
-			console.log('SW failed');
 
-		}
-	}
